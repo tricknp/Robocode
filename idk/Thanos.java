cda072aa4513084eac40c89ed8a1280d76400260
tricknp <np_patrick@yahoo.com>
2018-07-01 03:28:39 -0300

    adding all robots

---
+++ idk/Thanos.java
@@ -0,0 +1,108 @@
+package idk;
+
+import java.awt.Color;
+
+import robocode.AdvancedRobot;
+import robocode.HitByBulletEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.ScannedRobotEvent;
+import robocode.WinEvent;
+
+public class Thanos extends AdvancedRobot{ // extends para criaï¿½ï¿½o de robos avanï¿½ados
+
+	
+	int movimentar = 1;
+	
+	public void run() { // implementando run, classe principal
+			
+		setAdjustRadarForRobotTurn(true); // fica com o radar ativo sempre buscando inimigo
+		
+		setBodyColor(Color.black);
+		setGunColor(Color.red);
+		setRadarColor(Color.red);
+		setScanColor(Color.red);
+		setBulletColor(Color.yellow);
+		
+		setAdjustGunForRobotTurn(true); // mantem a arma pronta para quando achar o inimigo 
+		
+		turnRadarRightRadians(Double.POSITIVE_INFINITY); // fica girando o radar sem parar para a direita
+				
+	}
+	/*
+	 * Funcao que escaneia os robos
+	 */	
+	public void onScannedRobot(ScannedRobotEvent e) {
+		// criando uma variavel double que recebe a distancia do robo escaneado mais a distancia do nosso robo
+		double distanciaNRxRI=e.getBearingRadians()+getHeadingRadians(); 
+		// criando uma variavel double que guarda a velocidade do inimigo
+		double velocidadeInimigo=e.getVelocity() * Math.sin(e.getHeadingRadians() -distanciaNRxRI);
+		// criando uma variavel que recebera a posicao para o disparo
+		double ajustaMira;
+		setTurnRadarLeftRadians(getRadarTurnRemainingRadians());
+		if(Math.random()>.9){
+			setMaxVelocity((12*Math.random())+12);
+		}
+		/*
+		 * Abaixo o codigo para medir a distancia do inimigo para o disparo 
+		 */
+		if (e.getDistance() > 150) { // se a distancia do inimigo for menor que 250e maior que 100
+			// ajusta a mira com a formula da distancia do nosso robo menos a distancia do rovo inimigo mais a 
+			// velociadade do robo inimigo, tudo isso dividido por 22
+			ajustaMira = robocode.util.Utils.normalRelativeAngle(distanciaNRxRI- getGunHeadingRadians()+velocidadeInimigo/22);
+			// ajusta o canhao para o desparo
+			setTurnGunRightRadians(ajustaMira); 
+			setTurnRightRadians(robocode.util.Utils.normalRelativeAngle(distanciaNRxRI-getHeadingRadians()+velocidadeInimigo/getVelocity()));
+			// anda de atras do robo inimigo baseado na formula que 
+			setAhead((e.getDistance() - 140)*movimentar);
+			// atira com o nivel 2 de potencia
+			setFire(2); 
+		}
+		else{ // esse else e para quando a distancia for menor que 99
+			// ajusta a mira com a formula da distancia do nosso robo menos a distancia do rovo inimigo mais a 
+			// velociadade do robo inimigo, tudo isso dividido por 22
+			ajustaMira = robocode.util.Utils.normalRelativeAngle(distanciaNRxRI- getGunHeadingRadians()+velocidadeInimigo/15);
+			// ajusta o canhao para o desparo
+			setTurnGunRightRadians(ajustaMira);
+			setTurnLeft(-90-e.getBearing()); 
+			// anda de atras do robo inimigo baseado na formula que 
+			setAhead((e.getDistance() - 140)*movimentar);
+			// atira com o nivel 3 de potencia
+			setFire(3); 
+		}	
+	}
+	/*
+	 * Funcao acionada quando o robo colide com uma parede
+	 */
+	public void onHitWall(HitWallEvent e){
+		movimentar=-movimentar; // a variavel movimentar recebe o valor dela menos 1
+	}
+	/*
+	 * Funcao acionada quando o robo se colide com outro 
+	 */
+	public void onHitRobot(HitRobotEvent e){
+		setAhead(movimentar*55); // se movimenta para, se afastar do inimigo		
+	}
+	/*
+	 * Quando o nosso robo e acertado 
+	 */
+	public void onHitByBullet(HitByBulletEvent e) {
+		//setAhead(movimentar*55+10);  // se movimenta para, se afastar do inimigo
+		fire(3);
+	}	
+	/*
+	 * Funcao acionada quando ele vence uma batalha (fica somente ele vivo no campo) 
+	 */
+	public void onWin(WinEvent e) {
+		// laco de repeticao for que se repete 50 vezes
+		for (int i = 0; i < 50; i++) {
+			setFire(3);
+			setTurnRight(30); // gira para direita
+			setFire(3);
+			setTurnLeft(30); // gira para esquerda
+		}
+	}
+
+	
+	
+}
---
+++ idk/Thor.java
@@ -0,0 +1,326 @@
+package idk;
+import java.awt.Color;
+import robocode.AdvancedRobot;
+import robocode.Condition;
+import robocode.CustomEvent;
+import robocode.HitRobotEvent;
+import robocode.HitWallEvent;
+import robocode.RobotDeathEvent;
+import robocode.Rules;
+import robocode.ScannedRobotEvent;
+import robocode.util.Utils;
+
+public class Thor extends AdvancedRobot {
+
+	Enemy enemy = new Enemy();
+  	Robot robot;
+
+  int direction;
+	int muitoProximoParede = 0;
+	int limiteParede = 65;
+	boolean desvia = false;
+	double BF_WIDTH, BF_HEIGHT;
+
+	double tick = 0;
+
+  final double limite = 300; // Limite utilizado para troca de direção
+
+	int shots = 0;
+  int a = 0;
+
+	@Override
+	public void run() {
+
+		BF_HEIGHT = getBattleFieldHeight();
+		BF_WIDTH = getBattleFieldWidth();
+		direction = 1;
+		robot = new tankWave();
+
+		// Verifica se o inimigo deu um tiro (possivelmente) a partir da analise
+		// da energia.
+		addCustomEvent(new Condition() {
+			public boolean test() {
+				if (!enemy.none()) {
+					double energyDiff = enemy.getOldEnergy() - enemy.getEnergy();
+					return energyDiff >= .1 && energyDiff <= 3;
+				}
+				return false;
+			}
+		});
+
+
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
+		setAdjustRadarForRobotTurn(true);
+
+		// limpa o nome de qualquer inimigo armazenado, para iniciar o round.
+		enemy.reset();
+
+		// faz um primeiro scan para pegar um inimigo.
+		setTurnRadarRight(360);
+
+		do {
+
+			// mantem controle do tempo que passou, e se foi maior que 20 ticks,
+			// faz um rapido scan (pra tentar pegar algum inimigo que esteja
+			// mais proximo, e corrigir o bug que perde o radar em inimigos
+			// Robot (simples))
+			if (getTime() - tick > 20) {
+				tick = getTime();
+				setTurnRadarRight(360);
+				scan();
+			}
+
+			// inicializa a classe referente ao corpo do robot
+			robot.init();
+			robot.move();
+
+			// executar threads
+			execute();
+		} while (true);
+
+	}
+
+	@Override
+	public void onCustomEvent(CustomEvent event) {
+		// Caso o evento que verifica se o inimigo atirou � executado, tenta
+		// desviar da bala.
+		if (getOthers() <= 3 && (event.getCondition().getName().equals("atirou"))) {
+			setAhead(49 * (-direction));
+		}
+		// Caso o evento que verifica a distancia da parede seja executado, para
+		// de andar. (no metodo 'move' ele irá verificar a velocidade que o robo
+		// esta andando, caso estiver parado, altera a direcao e retoma o
+		// movimento.
+		if (event.getCondition().getName().equals("parede")) {
+			if (muitoProximoParede <= 0) {
+				muitoProximoParede += limiteParede;
+				setMaxVelocity(0);
+			}
+		}
+	}
+
+	// funcao util para normalizacao do bearing de 180/-180 para 0/360.
+	static double normalizeBearing(double angle) {
+		while (angle > 180)
+			angle -= 360;
+		while (angle < -180)
+			angle += 360;
+		return angle;
+	}
+
+	// Se um robo morrer, verifica se é o que estava atacando, caso for, reseta
+	// a variavel que estava mantendo suas informacoes, e escaneia por um novo
+	// inimigo.
+	@Override
+	public void onRobotDeath(RobotDeathEvent event) {
+		if (enemy.getName().equals(event.getName())) {
+			enemy.reset();
+			setTurnRadarRight(360);
+		}
+	}
+
+	// Caso escanear um robo..
+	@Override
+	public void onScannedRobot(ScannedRobotEvent event) {
+		// verifica se ele tem energia, se n�o tiver, enche a bunda dele de tiro
+		// forca 1.3
+		if (event.getEnergy() == 0) {
+			setTurnGunRightRadians(Utils
+					.normalRelativeAngle(getHeadingRadians() - getGunHeadingRadians() + event.getBearingRadians()));
+			if (getTurnRemainingRadians() <= 5)
+				fire(1.3);
+		}
+
+		// Se nao, verifica se estamos perseguindo alguem ja, se o inimigo
+		// escaneado é o mesmo que estamos perseguindo, ou se este novo inimigo
+		// esta mais proximo que o nosso atual.
+		if (enemy.none() || enemy.getName().equals(event.getName()) || enemy.getDistance() > event.getDistance()) {
+			// caso for alguem mais proximo, ou o nosso inimigo atual, ou nao
+			// estivermos
+			// cacando ninguem, atualiza as informacoes do inimigo.
+			enemy.set(event, this);
+		}
+
+		// Fixa o radar no inimigo
+		setTurnRadarRight(Utils.normalRelativeAngleDegrees(getHeading() - getRadarHeading() + enemy.getBearing()));
+
+		// mantem o canhao mirado onde ele podera estar num instante futuro.
+		setTurnGunRightRadians(enemy.getShootMeHere() * 0.85);
+
+		// se o canhao estiver em posicao, atire.
+		if (getGunTurnRemainingRadians() <= 1) {
+			setFire(enemy.getFirePower());
+		}
+	}
+
+	// Caso bater em outro robo, mira nele e atira que nem louco.
+	@Override
+	public void onHitRobot(HitRobotEvent event) {
+		setTurnGunRightRadians(getHeadingRadians() - getGunHeadingRadians() + event.getBearingRadians());
+		if (getGunTurnRemainingRadians() < 5)
+			setFire(3);
+	}
+
+	// metodo ativado quando o robô bate na parede
+	@Override
+	public void onHitWall(HitWallEvent event) {
+	}
+
+	// as partes do nosso robo ('rodas')
+	class tankWave implements Robot {
+		@Override
+		public void init() {
+			setBodyColor(Color.black);
+			setGunColor(Color.red);
+			setRadarColor(Color.red);
+			setScanColor(Color.red);
+			setBulletColor(Color.yellow);
+		}
+		@Override
+		public void move() {
+			setTurnRight(10000);
+			setMaxVelocity(10);
+			ahead(10000);
+			setTurnRight(10000);
+			back(10000);
+			// mantem-se perpendicular a um angulo de 90 graus, com um offset de
+			// 16 graus, se aproximando gradativamente do inimigo.
+			setTurnRight(normalizeBearing(enemy.getBearing() + 90 - (1 * direction)));
+		}
+	}
+}
+
+//--------------------------------------------------------------------------------------
+
+// Interface para as partes dos robos.
+interface Robot {
+	public void init();
+	public void move();
+}
+
+// Classe auxiliar para armazenar as informacoes uteis sobre o robo inimigo.
+// Entre outros metodos que facilitam a vida.
+final class Enemy {
+	private String name;
+	private double heading, bearing, velocity, energy, headingRadians, bearingRadians, distance;
+
+	double energyOld;
+	double absBearingRadians;
+	double displacement;
+	private double X, Y;
+
+	double firePower;
+	double shootMeHere;
+
+	public Enemy() {
+	}
+
+	public Enemy(ScannedRobotEvent e, AdvancedRobot r) {
+		set(e, r);
+	}
+
+	public void reset() {
+		this.name = "";
+	}
+
+	public boolean none() {
+		return this.getName().isEmpty();
+	}
+
+	// Mantem controle da energia atual e anterior do robo. (para ver se ele ta
+	// atirando em mim)
+	private void setEnergy(double energyNew) {
+		this.energyOld = this.energy;
+		this.energy = energyNew;
+	}
+
+	public void set(ScannedRobotEvent e, AdvancedRobot r) {
+		this.name = e.getName();
+		this.heading = e.getHeading();
+		this.headingRadians = e.getHeadingRadians();
+		this.bearing = e.getBearing();
+		this.bearingRadians = e.getBearingRadians();
+		this.velocity = e.getVelocity();
+		setEnergy(e.getEnergy());
+		this.distance = e.getDistance();
+
+		// posicao absoluta em relacao ao meu robo
+		this.absBearingRadians = r.getHeadingRadians() + this.bearingRadians;
+
+		// que forc devo atirar nesse inimigo
+		this.firePower = Math.max(r.getBattleFieldHeight(), r.getBattleFieldWidth()) / getDistance();
+
+		// calcula posicao X e Y do inimigo
+		this.X = r.getX() + Math.sin(getAbsBearingRadians()) * getDistance();
+		this.Y = r.getY() + Math.cos(getAbsBearingRadians()) * getDistance();
+
+		// calcula deslocamento
+		this.displacement = getVelocity() * Math.sin(getHeadingRadians() - getAbsBearingRadians())
+				/ Rules.getBulletSpeed(getFirePower());
+
+		// onde devo atirar
+		this.shootMeHere = Utils
+				.normalRelativeAngle(getAbsBearingRadians() - r.getGunHeadingRadians() + getDisplacement());
+	}
+
+	public double getShootMeHere() {
+		return shootMeHere;
+	}
+
+	public double getFirePower() {
+		return firePower;
+	}
+
+	public double getAbsBearingRadians() {
+		return absBearingRadians;
+	}
+
+	public double getDisplacement() {
+		return displacement;
+	}
+
+	public double getX() {
+		return X;
+	}
+
+	public double getY() {
+		return Y;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getHeading() {
+		return heading;
+	}
+
+	public double getBearing() {
+		return bearing;
+	}
+
+	public double getVelocity() {
+		return velocity;
+	}
+
+	public double getOldEnergy() {
+		return energyOld;
+	}
+
+	public double getEnergy() {
+		return energy;
+	}
+
+	public double getHeadingRadians() {
+		return headingRadians;
+	}
+
+	public double getBearingRadians() {
+		return bearingRadians;
+	}
+
+	public double getDistance() {
+		return distance;
+	}
+}
